e8f3393340e5fef9f52247b74f9e7454bd405bd8
Blaine Schmeisser <blainesch@gmail.com>
2016-07-15 10:29:45 -0700

    Make a zazu plugin.

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Zazu File Finder
+
+Find files and applications on your computer easily.
+
+## Commands
+
+~~~
+> find readme
+> open readme
+~~~
+
+You can also find applications without a prefix by just typing in part of the
+name:
+
+~~~
+> chrome
+~~~
---
+++ zazu.js
@@ -0,0 +1,50 @@
+module.exports = {
+  name: 'File Finder',
+  version: '0.0.1',
+  author: 'blainesch',
+  description: 'Find files and applications on your computer easily.',
+  homepage: 'https://github.com/tinytacoteam/zazu-file-finder',
+  git: 'git@github.com:tinytacoteam/zazu-file-finder.git',
+  install: 'npm install',
+  blocks: {
+    input: [
+      {
+        id: 1,
+        type: 'RootScript',
+        respondsTo: (input) => {
+          return true
+        },
+        script: 'node appFinder.js "{query}"',
+        connections: ['open'],
+      },
+      {
+        id: 2,
+        type: 'PrefixScript',
+        prefix: 'find',
+        space: true,
+        args: 'Required',
+        script: 'node fileFinder.js "{query}"',
+        connections: ['find'],
+      },
+      {
+        id: 3,
+        type: 'PrefixScript',
+        prefix: 'open',
+        space: true,
+        args: 'Required',
+        script: 'node fileFinder.js "{query}"',
+        connections: ['open'],
+      },
+    ],
+    output: [
+      {
+        id: 'open',
+        type: 'OpenFile',
+      },
+      {
+        id: 'find',
+        type: 'ShowFile',
+      },
+    ],
+  },
+}
