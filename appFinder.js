4ab578eff1d0a691ad3db90d2f7601105ae2e273
Blaine Schmeisser <blainesch@gmail.com>
2016-07-15 00:24:34 -0700

    Add a app finder.

---
+++ appFinder.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const os = require('os')
+const Finder = require('./lib/finder')
+const query = process.argv.slice(-1)[0]
+const regex = new RegExp(query, 'i')
+
+const finder = new Finder({
+  includePath: [
+    path.join(os.homedir()),
+  ],
+  excludePath: [
+    path.join(os.homedir(), 'Library'),
+  ],
+  excludeName: [
+    'node_modules',
+    'bower_components',
+    'vendor',
+    'tmp',
+    'tags',
+    'log',
+  ],
+})
+
+finder.deepFind().then((files) => {
+  return files.filter((file) => {
+    return file.name.match(regex) && file.name.match(/\.app$/)
+  })
+}).then((matchedFiles) => {
+  console.log(matchedFiles.slice(0, 9).map((file) => {
+    return file.toJson()
+  }))
+})
