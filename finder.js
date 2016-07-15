aeebb8b43b9582ce5968ee4bca9bc99a9336cc5c
Blaine Schmeisser <blainesch@gmail.com>
2016-07-15 00:22:47 -0700

    Remove broken files from search results.

--- finder.js
+++ finder.js
@@ -2,6 +2,7 @@ const path = require('path')
 const os = require('os')
 const Finder = require('./lib/finder')
 const query = process.argv.slice(-1)[0]
+const regex = new RegExp(query, 'i')
 
 const finder = new Finder({
   includePath: [
@@ -22,7 +23,7 @@ const finder = new Finder({
 
 finder.deepFind().then((files) => {
   return files.filter((file) => {
-    return file.name.match(query)
+    return file.name.match(regex)
   })
 }).then((matchedFiles) => {
   console.log(matchedFiles.slice(0, 9).map((file) => {
