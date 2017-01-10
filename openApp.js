068dec0bb1ea3cdb50d33e4b6e5f79c95834878a
Jeroen Boersma <jeroen@srcode.nl>
2017-01-10 21:10:03 +0100

    Start apps directly with spawning
    + starting from a fork throws a lot of errors (on Franzapp, Nylas)
    + show errors in console

--- openApp.js
+++
@@ -1,5 +0,0 @@
-const app = process.argv.slice(-1)[0]
-const cp = require('child_process')
-
-cp.exec(app)
-process.exit(0)
--- openAppManager.js
+++ openAppManager.js
@@ -1,14 +1,27 @@
-const { fork } = require('child_process')
+const spawn = require('child_process').spawn
+const os = require('os')
 const path = require('path')
 
 module.exports = (pluginContext) => {
   const { cwd } = pluginContext
-  return (value, env = {}) => {
+  return (app, env = {}) => {
     return new Promise((resolve, reject) => {
       if (process.platform === 'win32' || process.platform === 'darwin') {
         return resolve(value)
       }
-      fork(path.join(cwd, 'openApp.js'), [value])
+
+      // Use absolute paths at all time
+      if (app.substring(0, 1) == '~') {
+        app = os.homedir() + app.substring(1);
+      }
+
+      var p =  spawn(app, [], {
+        cwd: os.homedir()
+      })
+      p.stderr.on('data', (data) => {
+        console.error('err (' + path.basename(app) + '): ' + data);
+      })
+
       reject() // break chain
     })
   }
