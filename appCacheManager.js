2bb0054acc5605816bbf386b08eb55aacac0e5bc
Jeroen Boersma <jeroen@srcode.nl>
2017-01-11 14:25:13 +0100

    Default value for options if onmitted

--- appCacheManager.js
+++ appCacheManager.js
@@ -4,8 +4,7 @@ const path = require('path')
 module.exports = (pluginContext) => {
   const { cwd } = pluginContext
   const appCacheProcess = path.join(cwd, 'appCache.js')
-  return (options) => {
-    const {directories} = options;
+  return (options = {}) => {
     const runner = fork(appCacheProcess, [cwd, JSON.stringify(options)])
     return new Promise((resolve) => {
       runner.on('exit', resolve)
