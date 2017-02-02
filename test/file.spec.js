8e36d824dc6b3a17fec0517bf543cb2da696c58d
Tao Wang <twang2218@gmail.com>
2017-02-02 18:37:53 +1100

    Refactor the app info/icon loading code to be more testable
    Signed-off-by: Tao Wang <twang2218@gmail.com>

--- test/file.spec.js
+++ test/file.spec.js
@@ -5,69 +5,6 @@ const os = require('os')
 
 const File = require('../lib/file')
 
-class StubFile extends File {
-  isDirectory () {
-    return false
-  }
-  loadInfo () {
-    this.info = {}
-    this.iconPath = ''
-  }
-}
-
-describe('File.findBestIcon() should return size "128" if it exists', (assert) => {
-  const iconSetWithout2x = [
-    'icon_128x128.png',
-    'icon_16x16.png',
-    'icon_256x256.png',
-    'icon_32x32.png',
-    'icon_512x512.png',
-  ]
-  const iconSetWith2x = [
-    'icon_128x128@2x.png',
-    'icon_128x128.png',
-    'icon_16x16.png',
-    'icon_256x256.png',
-    'icon_32x32.png',
-    'icon_512x512.png',
-    'icon_16x16@2x.png',
-    'icon_256x256@2x.png',
-    'icon_32x32@2x.png',
-  ]
-
-  const f = new StubFile('Terminal.app', '/Applications/Utilities')
-  assert.equal(f.findBestIcon(iconSetWithout2x), 'icon_128x128.png')
-  assert.equal(f.findBestIcon(iconSetWith2x), 'icon_128x128.png')
-  assert.end()
-})
-
-describe('File.findBestIcon() should return size "256" if it doesn\'t exist and have 256', (assert) => {
-  const iconSetWithout128 = [
-    'icon_16x16.png',
-    'icon_256x256.png',
-    'icon_32x32.png',
-    'icon_512x512.png',
-  ]
-
-  const f = new StubFile('Terminal.app', '/Applications/Utilities')
-  assert.equal(f.findBestIcon(iconSetWithout128), 'icon_256x256.png')
-  assert.end()
-})
-
-describe('File.findBestIcon() should return largest size if no prefered size exists', (assert) => {
-  const iconSetWithout128and256 = [
-    'icon_16x16.png',
-    'icon_32x32.png',
-    'icon_512x512.png',
-    'icon_16x16@2x.png',
-    'icon_32x32@2x.png',
-  ]
-
-  const f = new StubFile('Terminal.app', '/Applications/Utilities')
-  assert.equal(f.findBestIcon(iconSetWithout128and256), 'icon_512x512.png')
-  assert.end()
-})
-
 //  Do the real world test on macOS system
 if (process.platform === 'darwin') {
   const testCwd = path.join(os.tmpdir(), 'test', 'file')
--- test/index.js
+++ test/index.js
@@ -1,3 +1,4 @@
 require('./appFinder.spec')
 require('./fileFinder.spec')
+require('./appInfo.spec')
 require('./file.spec')
